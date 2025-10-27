b90eef51b218837a78902507159543f7f9d24deb
EdwardSnug <prezokams@gmail.com>
2025-10-27 09:15:23 +0300

    Fixing Jest router duplication and axios transform issue

--- frontendfarmart/jest.config.js
+++ frontendfarmart/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: "jsdom",
   transformIgnorePatterns: [
-    "node_modules/(?!(axios)/)"
+    "node_modules/(?!(axios|react-router-dom)/)" // Allow Jest to process these ESM modules
   ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // avoid CSS import errors
+  },
 };
--- frontendfarmart/src/App.test.js
+++ frontendfarmart/src/App.test.js
@@ -1,16 +1,29 @@
+jest.mock('axios', () => {
+  const mockAxios = {
+    get: jest.fn(),
+    post: jest.fn(),
+    create: jest.fn(() => mockAxios),
+    interceptors: {
+      request: { use: jest.fn(), eject: jest.fn() },
+      response: { use: jest.fn(), eject: jest.fn() },
+    },
+  };
+  return mockAxios;
+});
+
+// Silence console.log for cleaner test output
+jest.spyOn(console, 'log').mockImplementation(() => {});
+
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 test('renders App component without crashing', () => {
   render(
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   );
 });
